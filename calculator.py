861ac54b5daf65c0bfe001abb4e3d8f727872b4b
Alexandre Coimbra <alexandrewcoimbra@gmail.com>
2023-09-17 23:34:47 -0300

    adicionando feature de soma

---
+++ calculator.py
@@ -0,0 +1,11 @@
+def soma(n1: int, n2: int) -> int:
+    """
+    Soma dois números
+    Args:
+    n1: primeiro número;
+    n2: segundo numero;
+
+    Returns:
+    A soma de dois números.
+    """
+    return n1 + n2
