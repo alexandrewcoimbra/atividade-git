64c9dedf07c41d4e7fc80f8402d06bb08555e070
Alexandre Coimbra <alexandrewcoimbra@gmail.com>
2023-09-17 23:35:53 -0300

    adicionando feature de subtração

--- calculator.py
+++ calculator.py
@@ -1,11 +1,12 @@
-def soma(n1: int, n2: int) -> int:
+def subtracao(n1: int, n2: int) -> int:
     """
-    Soma dois números
+    Subtrai dois números
     Args:
     n1: primeiro número;
     n2: segundo numero;
 
     Returns:
-    A soma de dois números.
+    A subtração de dois números.
     """
-    return n1 + n2
+    return n1 - n2
+
