070013a1b3a4d0fe3894a78521c20a3d09441714
Alexandre Coimbra <alexandrewcoimbra@gmail.com>
2023-09-17 23:38:54 -0300

    adicionando feature de divisão

--- calculator.py
+++ calculator.py
@@ -1,11 +1,11 @@
-def multiplicacao(n1: int, n2: int) -> int:
+def divisao(n1: int, n2: int) -> float:
     """
-    Multiplica dois números
+    Divide dois números
     Args:
     n1: primeiro número;
     n2: segundo numero;
 
     Returns:
-    A multiplicação de dois números.
+    A divisão de dois números.
     """
-    return n1 * n2
+    return n1 / n2
